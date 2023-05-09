5eed3715f10deea633731f3fa441ad1f036443ee
justina177 <justinaenenche@yahoo.com>
2023-05-09 14:49:50 +0100

    Added server and added mongodb

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const cors = require('cors');
 
 const app = express()
 
+
 const PORT = process.env.PORT || 5000;
 
 // Connect to Db and Start Server
 mongoose
-    .connect(procee.env.MONGO_URI)
+    .connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(PORT, ()=> {
+        app.listen(PORT, () => {
             console.log(`Server Running on ${PORT}`)
         })
 
