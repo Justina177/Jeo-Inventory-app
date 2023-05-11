44106ba15ff8add05608cb6e65652a6265ee6a22
justina177 <justinaenenche@yahoo.com>
2023-05-11 02:00:25 +0100

    Added logout user funcionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,7 +116,23 @@ const loginUser = asyncHandler( async (req, res) => {
     }
 });
 
+//  Logout User
+const logout = asyncHandler( async (req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0), // 1 day
+        sameSite: "none",
+        secure: true
+    });
+    return res.status(200).json({ message: "successfully Logout"})
+    
+});
+
+
+
 module.exports = {
     registerUser,
     loginUser,
+    logout,
 };
